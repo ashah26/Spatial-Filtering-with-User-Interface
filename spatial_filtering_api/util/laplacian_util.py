01b95284935478c543071f87a0e1615d89679fab
amrutaghodke <31553735+amrutaghodke@users.noreply.github.com>
2018-11-27 11:36:41 -0600

    Minor changes

--- spatial_filtering_api/util/laplacian_util.py
+++ spatial_filtering_api/util/laplacian_util.py
@@ -4,18 +4,20 @@ import random
 
 MAX_NO = 99999
 
-
-def apply_laplacian_filter(img, filter, negmul=False):
+def ApplyFilter(img, filter):
     height = img.shape[0]
     width = img.shape[1]
 
-    print(height, width)
+    print(height , width)
 
     filterheight = filter.shape[0]
     filterwidth = filter.shape[1]
 
-    #padded image
+    if height < filterheight:
+        print("height < filterhright, not proper filter")
+        return 0
 
+    #padded image dimension
     startpoint = int(filterheight/2)
     endpoint = int(filterheight/2) * -1
     rowpadding = int(filterheight/2)*2
@@ -31,8 +33,13 @@ def apply_laplacian_filter(img, filter, negmul=False):
     print(mask.shape[0] , mask.shape[1])
 
     mulby = 1
-    if negmul == True:
+    index = int(filterheight/2)
+    print("middle value : ", filter[index, index])
+    if filter[index, index] < 0:
+        print("Setting mulby -1")
         mulby = -1
+    else:
+        print("Setting mulby 1")
 
     #print(img_out)
     for i in np.arange(0, height):
@@ -47,20 +54,19 @@ def apply_laplacian_filter(img, filter, negmul=False):
             mask.itemset((i, j), b)
 
     print(i , j)
-    return mask
 
-def WriteImages(img, filter):
-    final = img + filter
-    #final = img_out
+    finalimag = img + mask
+    return finalimag
+
+def WriteImages(finalimag):
+
     no = random.randint(0, MAX_NO)
     print(no)
     name = "finalimg" + str(no) + ".jpg"
-    filtername = "filter" + str(no) + ".jpg"
-
     print(name)
 
-    cv2.imwrite(name, final)
-    cv2.imwrite(filtername, filter)
-    print("Images are written to disk", name, " & ", filtername)
+    cv2.imwrite(name, finalimag)
+
+    print("Images are written to disk", name)
 
 
