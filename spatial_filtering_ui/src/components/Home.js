7108afa6cb674a1b8b4e9b504ce7fa1dcf2d1810
ashah26 <ashna.shah.1426@gmail.com>
2018-12-02 18:32:52 -0600

    Download Functionality Implemented

--- spatial_filtering_ui/src/components/Home.js
+++ spatial_filtering_ui/src/components/Home.js
@@ -1,7 +1,10 @@
 import React,{Component} from 'react';
+import "downloadjs";
 import {fetchFilterData, performFilter} from "./../api/API";
 import SelectFilter from "./SelectFilter";
 import notavailable from './../assets/images/not_available.jpg';
+import '../assets/css/Home.css';
+// import LoadingSpinner from "./LoadingSpinner"
 
 
 class Home extends Component{
@@ -70,6 +73,7 @@ class Home extends Component{
             return (
                 <div className="custom-control custom-radio">
                     <input type="radio" className="custom-control-input" id={filter_type} name="filter_type"
+                           checked={this.state.selected_filter_type === filter_type}
                            onClick={(()=>{
                                this.setState({
                                    ...this.state,
@@ -168,6 +172,7 @@ class Home extends Component{
         this.setState({
             ...this.state,
             selected_mask_dict: mask
+
         })
     });
     
@@ -197,10 +202,19 @@ class Home extends Component{
         })
     });
 
+    downloadImage = (()=>{
+        if(this.state.filtered_image){
+            var url = this.state.filtered_image.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
+            window.open(url);
+
+        }
+    });
+
     render() {
         return(
             <div className="container-fluid">
-                <label className="h1">Welcome to Digital Processing</label>
+                <label className="h1">Welcome to Digital Image Processing</label><br/>
+                <label className="h2">Spatial Filtering</label>
                 <div className="row">
                     <div className="col-md-4 col-lg-4 col-sm-4 col-xs-4">
                         {this.showFilterTypes()}
@@ -208,17 +222,17 @@ class Home extends Component{
                     <div className="col-md-4 col-lg-4 col-sm-4 col-xs-4" align="left">
                         {this.showFilters()}
                     </div>
-                    <div>
+                    <div class="select_mask_button">
                         <button id="btn_select_filter" type="button" className="btn btn-primary"
                                 disabled={!(this.state.selected_filter_type && this.state.selected_filter)}
                                 data-toggle="modal"
                                 data-target="#exampleModalLong"
                             >
-                            Select Filter
+                            Select Mask
                         </button>
                     </div>
 
-                    <div>
+                    <div class="reset_button">
                         <button id="btn_select_filter" type="button" className="btn btn-primary"
                                 onClick={(()=>{
                                     this.setState({
@@ -239,14 +253,14 @@ class Home extends Component{
                 </div>
 
                 <div className="row">
-                    <div className="col-sm-6 col-lg-6 col-md-6 col-xs-6">
+                    <div className="col-sm-6 col-lg-6 col-md-6 col-xs-6 file_path">
                         <div className="input-group mb-3">
                             <input type="text" value={this.state.file_path} className="form-control" placeholder="File Path" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                             <button className="btn btn-primary" onClick={(()=>{document.getElementById("fileBrowse").click()})}>Browse</button>
                             <input type="file" id="fileBrowse" hidden={true} onChange={((event)=>{this.uploadImage(event)})}/>
                         </div>
                     </div>
-                    <div className="col-sm-6 col-lg-6 col-md-6 col-xs-6">
+                    <div className="col-sm-6 col-lg-6 col-md-6 col-xs-6 file_path">
                         <button type="button" className="btn btn-primary"
                                 disabled={!(this.state.original_image && this.state.selected_mask_dict.mask)}
                                 onClick={(()=>{this.performFilter()})}> Apply Filter</button>
@@ -257,16 +271,26 @@ class Home extends Component{
                         <img className="img-fluid" src={(this.state.original_image ? this.state.original_image : notavailable)} alt="original pic"/>
                     </div>
                     <div className="col-sm-6 col-lg-6 col-md-6 col-xs-6">
-                        <img className="img-fluid" src={(this.state.filtered_image ? this.state.filtered_image : notavailable)} alt="filtered pic"/>
+                        <img className="img-fluid"
+                             src={(this.state.filtered_image ? this.state.filtered_image : notavailable)}
+                             alt="filtered pic"/>
                     </div>
                 </div>
+
+                <div className="row file_path text-center">
+
+                       <button type="button" className="btn btn-primary center-block"
+                            onClick={(()=>{this.downloadImage()})}> Download
+                       </button>
+                </div>
+                <a hidden={true} id="dwnldLnk"></a>
                 <div>
                     <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog"
                          aria-labelledby="exampleModalLongTitle" aria-hidden="true">
                         <div className="modal-dialog" role="document">
                             <div className="modal-content">
                                 <div className="modal-header">
-                                    <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                    <h5 className="modal-title" id="exampleModalLongTitle">Select Mask</h5>
                                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
--- spatial_filtering_ui/src/components/SelectFilter.js
+++ spatial_filtering_ui/src/components/SelectFilter.js
@@ -38,7 +38,7 @@ class SelectFilter extends Component{
                         <div className="col-lg-1 col-md-1 col-sm-1 col-xs-1">
                             <input type="radio" className="custom-control-input" id={"mask"+mask_dict.id}
                                    name="mask_rdb"
-                                   checked={this.props.selected_mask.id === mask_dict.id}
+                                   checked={this.props.selected_mask_dict.id === mask_dict.id}
                                    onClick={(() => {
                                        this.props.handleMaskSelect(mask_dict);
                                    })}
@@ -46,9 +46,6 @@ class SelectFilter extends Component{
                             <label className="custom-control-label" htmlFor={"mask"+mask_dict.id}>
                                 {(mask_dict.hasOwnProperty("name") ? mask_dict.name : "")}
                             </label>
-                        </div>
-                        <div className="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                            {((mask_dict.weight === "0") ? "" : mask_dict.weight)}
                         </div>
                         <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
                             {
@@ -59,6 +56,7 @@ class SelectFilter extends Component{
                                                 row.map((column, index)=>{
                                                     return(
                                                         <div className="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                            {/* //TODO: add formating*/}
                                                             {column}
                                                         </div>
                                                     )
@@ -69,6 +67,19 @@ class SelectFilter extends Component{
                                 })
                             }
                         </div>
+
+                        {/*<div className="row">*/}
+
+                                {/*if(this.props.selected_filter == 'Unsharp Mask Filter'){*/}
+
+                                    {/*return(*/}
+                                        {/*<input type="text" value={this.props.selected_mask_dict.k} className="form-control"*/}
+                                        {/*aria-label="Recipient's username" aria-describedby="button-addon2"/>*/}
+                                    {/*)*/}
+
+                                {/*}*/}
+
+                        {/*</div>*/}
                     </div>
                 )
             })
